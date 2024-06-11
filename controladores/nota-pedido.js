ff2879465a1beffe1ccb5cf9a400856c421b6bf2
Walter Lauxmann <walterlauxmann@institutocedec.com>
2024-06-11 20:34:50 -0300

    04-Nota de pedido. Primera parte

---
+++ controladores/nota-pedido.js
@@ -0,0 +1,24 @@
+/* Objetos del DOM */
+// Códigos
+const codigo1 = document.getElementById('spancodigo1');
+const codigo2 = document.getElementById('spancodigo2');
+const codigo3 = document.getElementById('spancodigo3');
+const codigo4 = document.getElementById('spancodigo4');
+
+// Nombres
+const nombre1 = document.getElementById('spannombre1');
+const nombre2 = document.getElementById('spannombre2');
+const nombre3 = document.getElementById('spannombre3');
+const nombre4 = document.getElementById('spannombre4');
+
+// Precios
+const precio1 = document.getElementById('spanprecio1');
+const precio2 = document.getElementById('spanprecio2');
+const precio3 = document.getElementById('spanprecio3');
+const precio4 = document.getElementById('spanprecio4');
+
+// Cantidades
+const cantidad1 = document.getElementById('inputcantidad1');
+const cantidad2 = document.getElementById('inputcantidad2');
+const cantidad3 = document.getElementById('inputcantidad3');
+const cantidad4 = document.getElementById('inputcantidad4');
