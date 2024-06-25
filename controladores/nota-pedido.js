522d21b2526dedf91b5b4892e86c0d9bbf93d699
Walter Lauxmann <walterlauxmann@institutocedec.com>
2024-06-25 19:12:00 -0300

    05-Nota de pedido - segunda parte

--- controladores/nota-pedido.js
+++ controladores/nota-pedido.js
@@ -1,3 +1,4 @@
+function calcular() {
 /* Objetos del DOM */
 // Códigos
 const codigo1 = document.getElementById('spancodigo1');
@@ -22,3 +23,41 @@ const cantidad1 = document.getElementById('inputcantidad1');
 const cantidad2 = document.getElementById('inputcantidad2');
 const cantidad3 = document.getElementById('inputcantidad3');
 const cantidad4 = document.getElementById('inputcantidad4');
+
+// Variables
+let importe1 = precio1.innerHTML * cantidad1.value;
+let importe2 = precio2.innerHTML * cantidad2.value;
+let importe3 = precio3.innerHTML * cantidad3.value;
+let importe4 = precio4.innerHTML * cantidad4.value;
+
+let total = importe1 + importe2 + importe3 + importe4;
+
+// Mostrar los resultados en la tabla
+document.getElementById('codigo1').innerHTML = codigo1.innerHTML;
+document.getElementById('codigo2').innerHTML = codigo2.innerHTML;
+document.getElementById('codigo3').innerHTML = codigo3.innerHTML;
+document.getElementById('codigo4').innerHTML = codigo4.innerHTML;
+
+document.getElementById('nombre1').innerHTML = nombre1.innerHTML;
+document.getElementById('nombre2').innerHTML = nombre2.innerHTML;
+document.getElementById('nombre3').innerHTML = nombre3.innerHTML;
+document.getElementById('nombre4').innerHTML = nombre4.innerHTML;
+
+document.getElementById('cantidad1').innerHTML = cantidad1.value;
+document.getElementById('cantidad2').innerHTML = cantidad2.value;
+document.getElementById('cantidad3').innerHTML = cantidad3.value;
+document.getElementById('cantidad4').innerHTML = cantidad4.value;
+
+document.getElementById('precio1').innerHTML = precio1.innerHTML;
+document.getElementById('precio2').innerHTML = precio2.innerHTML;
+document.getElementById('precio3').innerHTML = precio3.innerHTML;
+document.getElementById('precio4').innerHTML = precio4.innerHTML;
+
+document.getElementById('importe1').innerHTML = importe1;
+document.getElementById('importe2').innerHTML = importe2;
+document.getElementById('importe3').innerHTML = importe3;
+document.getElementById('importe4').innerHTML = importe4;
+
+document.getElementById('total').innerHTML = total;
+
+}
