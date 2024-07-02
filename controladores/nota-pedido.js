36ace075041c5079df1e83be4af4463e20c2af64
Walter Lauxmann <walterlauxmann@institutocedec.com>
2024-07-02 18:44:27 -0300

    06-Nota de pedido con Array

--- controladores/nota-pedido.js
+++ controladores/nota-pedido.js
@@ -1,63 +1,40 @@
 function calcular() {
-/* Objetos del DOM */
-// Códigos
-const codigo1 = document.getElementById('spancodigo1');
-const codigo2 = document.getElementById('spancodigo2');
-const codigo3 = document.getElementById('spancodigo3');
-const codigo4 = document.getElementById('spancodigo4');
+  /* Objetos del DOM */
 
-// Nombres
-const nombre1 = document.getElementById('spannombre1');
-const nombre2 = document.getElementById('spannombre2');
-const nombre3 = document.getElementById('spannombre3');
-const nombre4 = document.getElementById('spannombre4');
+  const codigos = document.getElementsByName("spancodigo");  
+  const nombres = document.getElementsByName("spannombre");
+  const precios = document.getElementsByName("spanprecio");
+  const cantidades = document.getElementsByName("inputcantidad");
 
-// Precios
-const precio1 = document.getElementById('spanprecio1');
-const precio2 = document.getElementById('spanprecio2');
-const precio3 = document.getElementById('spanprecio3');
-const precio4 = document.getElementById('spanprecio4');
+  // Variables
+  let importes = [];
+  let total = 0;
 
-// Cantidades
-const cantidad1 = document.getElementById('inputcantidad1');
-const cantidad2 = document.getElementById('inputcantidad2');
-const cantidad3 = document.getElementById('inputcantidad3');
-const cantidad4 = document.getElementById('inputcantidad4');
+  // Inicio del bucle
+  for(let i=0; i < cantidades.length; i++) {
+    if(cantidades[i].value > 0) {
+        importes[i] = cantidades[i].value * precios[i].innerHTML;
+        total += importes[i]; // total = total + importes[i]
+    }
+  }
+  // Fin del bucle
 
-// Variables
-let importe1 = precio1.innerHTML * cantidad1.value;
-let importe2 = precio2.innerHTML * cantidad2.value;
-let importe3 = precio3.innerHTML * cantidad3.value;
-let importe4 = precio4.innerHTML * cantidad4.value;
+  // Mostrar los resultados en la tabla
+  document.getElementById('detalle').innerHTML = '';
+  for(let i=0; i < importes.length; i++) {
+    if(importes[i]) {
+        document.getElementById('detalle').innerHTML += `
+              <tr>
+                <td>${codigos[i].innerHTML}</td>
+                <td>${nombres[i].innerHTML}</td>
+                <td>${cantidades[i].value}</td>
+                <td>${precios[i].innerHTML}</td>
+                <td>${importes[i]}</td>
+              </tr>    
+    `
+    }
+    
+  }
 
-let total = importe1 + importe2 + importe3 + importe4;
-
-// Mostrar los resultados en la tabla
-document.getElementById('codigo1').innerHTML = codigo1.innerHTML;
-document.getElementById('codigo2').innerHTML = codigo2.innerHTML;
-document.getElementById('codigo3').innerHTML = codigo3.innerHTML;
-document.getElementById('codigo4').innerHTML = codigo4.innerHTML;
-
-document.getElementById('nombre1').innerHTML = nombre1.innerHTML;
-document.getElementById('nombre2').innerHTML = nombre2.innerHTML;
-document.getElementById('nombre3').innerHTML = nombre3.innerHTML;
-document.getElementById('nombre4').innerHTML = nombre4.innerHTML;
-
-document.getElementById('cantidad1').innerHTML = cantidad1.value;
-document.getElementById('cantidad2').innerHTML = cantidad2.value;
-document.getElementById('cantidad3').innerHTML = cantidad3.value;
-document.getElementById('cantidad4').innerHTML = cantidad4.value;
-
-document.getElementById('precio1').innerHTML = precio1.innerHTML;
-document.getElementById('precio2').innerHTML = precio2.innerHTML;
-document.getElementById('precio3').innerHTML = precio3.innerHTML;
-document.getElementById('precio4').innerHTML = precio4.innerHTML;
-
-document.getElementById('importe1').innerHTML = importe1;
-document.getElementById('importe2').innerHTML = importe2;
-document.getElementById('importe3').innerHTML = importe3;
-document.getElementById('importe4').innerHTML = importe4;
-
-document.getElementById('total').innerHTML = total;
-
+  document.getElementById("total").innerHTML = total;
 }
